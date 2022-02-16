68eab68adfe7f8b4010b52e3390d15c36b26a393
googie94 <sportfolio94@gmail.com>
2022-02-16 13:01:53 +0900

    edit settings

--- kyeolle/config/settings.py
+++ kyeolle/config/settings.py
@@ -2,19 +2,24 @@
 import json
 import sys
 import os
+from decouple import config
 
 # 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-CONFIG_SECRET_DIR = os.path.join(ROOT_DIR, '.config_secret')
-CONFIG_SECRET_BASE_FILE = os.path.join(CONFIG_SECRET_DIR, 'base.json')
+# CONFIG_SECRET_DIR = os.path.join(ROOT_DIR, '.config_secret')
+# CONFIG_SECRET_BASE_FILE = os.path.join(CONFIG_SECRET_DIR, 'base.json')
+# CONFIG_SECRET_BASE_FILE = os.path.join(CONFIG_SECRET_DIR, '.env')
 # CONFIG_SECRET_DEVELOPMENT_FILE = os.path.join(CONFIG_SECRET_DIR, 'development.json')
 # CONFIG_SECRET_PRODUCTION_FILE = os.path.join(CONFIG_SECRET_DIR, 'production.json')
-config_secret_base = json.loads(open(CONFIG_SECRET_BASE_FILE).read())
+# config_secret_base = json.loads(open(CONFIG_SECRET_BASE_FILE).read())
+
 
 # 
-SECRET_KEY = config_secret_base['django']['secret_key']
-DEBUG = config_secret_base['django']['debug']
+# SECRET_KEY = config_secret_base['django']['secret_key']
+SECRET_KEY = config("DJANGO_SECRET_KEY")
+# DEBUG = config_secret_base['django']['debug']
+DEBUG = config("DEBUG")
 ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -65,11 +70,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": config_secret_base['database']['name'],
-        "USER": config_secret_base['database']['user'],
-        "PASSWORD": config_secret_base['database']['password'],
-        "HOST": config_secret_base['database']['host'],
-        "PORT": config_secret_base['database']['port'],
+        # "NAME": config_secret_base['database']['name'],
+        # "USER": config_secret_base['database']['user'],
+        # "PASSWORD": config_secret_base['database']['password'],
+        # "HOST": config_secret_base['database']['host'],
+        # "PORT": config_secret_base['database']['port'],
+        "NAME": config("MYSQL_NAME"),
+        "USER": config("MYSQL_USER"),
+        "PASSWORD": config("MYSQL_PASSWORD"),
+        "HOST": config("MYSQL_HOST"),
+        "PORT": config("MYSQL_PORT")
     }
 }
 
